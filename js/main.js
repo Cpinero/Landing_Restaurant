91ff899ef2903784972d025c465a94e98a7247df
Cpinero <37397139+Cpinero@users.noreply.github.com>
2021-06-13 02:48:37 -0400

    landing page

---
+++ js/main.js
@@ -0,0 +1,101 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
+
+    // Valida si la variable existe
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            //Agregamos la clase al div con la clase nav__menu
+            nav.classList.toggle('show-menu');
+        });
+    }
+}
+showMenu('nav-toggle', 'nav-menu');
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    //Al hacer click em cada nav__link, removemos la clase show-menu
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+/*==================== SCROLL ACTIVE LINK ====================*/
+const section = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+
+/*==================== CAMBIAR BACKGROUND HEADER ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header');
+    // Cuando el scroll es mayor a 200 agregamos la clase
+    if (this.scrollY >= 200) nav.classList.add('scroll-header');
+    else nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // Cuando el scroll es mayor a 560 agregamos la clase
+    if (this.scrollY >= 560) scrollTop.classList.add('show-scroll');
+    else scrollTop.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollTop);
+
+/*==================== DARK LIGHT THEME ====================*/
+
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bx-sun';
+
+//Uso de localStorage para guardar el tema
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun';
+
+//cargamos el tema guardado en localStorage
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// al hacer click cambiamos el icono y guardamos en localStorage
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: ' 30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.home__data, .home__img , .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contect__data, .contact__button, .footer_content`, {
+    interval: 200
+})
