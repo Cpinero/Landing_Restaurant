5beed8ae34ceea8c03cb0ab7ccc9418a2af5f726
Cpinero <37397139+Cpinero@users.noreply.github.com>
2021-06-13 02:57:31 -0400

    Ajustes con el scrollReveal

--- js/main.js
+++ js/main.js
@@ -91,11 +91,16 @@ themeButton.addEventListener('click', () => {
 /*==================== SCROLL REVEAL ANIMATION ====================*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: ' 30px',
+    distance: '30px',
     duration: 2000,
     reset: true
 });
 
-sr.reveal(`.home__data, .home__img , .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contect__data, .contact__button, .footer_content`, {
+sr.reveal(`.home__data, .home__img,
+            .about__data, .about__img,
+            .services__content, .menu__content,
+            .app__data, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
     interval: 200
-})
+});
